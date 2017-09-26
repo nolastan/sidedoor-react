49066269f8708f287c282d992192aa2e58aa2d86
Stanford Rosenthal <nolastan@gmail.com>
2017-09-26 15:17:34 -0700

    airbnb calendar working with blocked dates

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import Calendar from './Calendar.js'
+import moment from 'moment';
+
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+class App extends Component {
+
+  constructor(){
+    super()
+    this.isDayBlocked = this.isDayBlocked.bind(this)
+  }
+
+  state = {
+    loading: true
+  }
+
+  componentWillMount() {
+    fetch(`http://localhost:3000/calendar.json?url=https://www.airbnb.com/calendar/ical/18123650.ics?s=fba76265b1bf3df892d6b38186762fea`)
+    // fetch(`http://sidedoor-rails.herokuapp.com/calendar.json?url=https://www.airbnb.com/calendar/ical/18123650.ics?s=fba76265b1bf3df892d6b38186762fea`)
+      .then(res => res.json())
+      .then(bookings => this.setState({loading: false, bookings: bookings}));
+  }
+
+  isDayBlocked(date) {
+    for (var i = 0; i < this.state.bookings.length; i++) {
+      if(moment(date).isBetween(this.state.bookings[i].begin, this.state.bookings[i].end)) {
+        return true
+      }
+    }
+    this.state.bookings.map(function(booking) {
+    })
+  }
+
+  render() {
+    if(this.state.loading) {
+      return (<div>Loading…</div>)
+    } else {
+      return (
+        <div className="App">
+          {this.state.bookings[21].begin}
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <DateRangePicker
+            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+            isDayBlocked={this.isDayBlocked}
+          />
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
